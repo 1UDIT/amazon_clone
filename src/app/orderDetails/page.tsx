74c8b8fca1210528335e202f329c146ec307d394
1UDIT <uditsharma7195@gmail.com>
2025-01-25 20:05:51 +0530

    fix use toast hook

--- src/app/orderDetails/page.tsx
+++ src/app/orderDetails/page.tsx
@@ -15,15 +15,15 @@ const Page = () => {
     const { data, error, isLoading, isError } = useQuery(['OrderFetch'], fetchData);
     console.log(data?.order, "data", isLoading);
     return (
-        <div className='w-full grid justify-center'>
-            <div className='max-w-full p-2 m-2 '>
+        <div className='w-full grid justify-center bg-white'>
+            <div className='w-full p-2 m-2'>
                 {
                     !isLoading && data.map((value: any, indx: any) => {
                         return (
                             <Fragment key={indx}>
                                 {value?.order.map((data: any) => {
                                     return (
-                                        <div className='border-2 boder-black rounded-xl border-solid grid grid-cols-[20%_80%] h-[10rem] items-center m-2 ' key={data.id}>
+                                        <div className='gap-6 border-2 boder-black rounded-xl border-solid grid grid-cols-[20%_80%] h-[10rem] items-center m-2 ' key={data.id}>
                                             <div className='h-[8rem]'>
                                                 <Image
                                                     className="cursor-pointer"
